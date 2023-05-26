2929e69eac846798c2ed92cb108901291bdb151c
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-05-26 21:41:53 +0200

    Update layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import {SquareInput} from "./components/SquareInput/SquareInput";
+import GitHub from './images/GitHub.png';
+import LinkdedIn from './images/LinkedIn.png';
 
 const App = () => {
     return (
@@ -49,9 +51,9 @@ const App = () => {
                 </div>
             </div>
             <div className={"bottom"}>
-                <p></p>
-                <a></a>
-                <a></a>
+                <p>Check my accounts</p>
+                <a target={"_blank"} href={"https://github.com/WeronikaCzepiela"}> <img src={GitHub}/> </a>
+                <a target={"_blank"} href={"https://www.linkedin.com/in/--weronika-czepiela/"}> <img src={LinkdedIn}/> </a>
             </div>
         </div>
     );
